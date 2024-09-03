cd11471906376300e7852a177fec97f6912665fe
daltonBergSoftware <daltonberg87@gmail.com>
2024-09-03 15:20:54 -0700

    Updates

---
+++ precache-manifest.e84c4421daad29fd0dc49e591f8c2b92.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0c7647e7a5f463ded36acc190c44c67",
+    "url": "/DaltonsReactPortfolioWeb/index.html"
+  },
+  {
+    "revision": "2bdf225cfe3ac8555a42",
+    "url": "/DaltonsReactPortfolioWeb/static/css/main.8d210233.chunk.css"
+  },
+  {
+    "revision": "a287299318f8196fb44c",
+    "url": "/DaltonsReactPortfolioWeb/static/js/2.7ad1f5c0.chunk.js"
+  },
+  {
+    "revision": "b9afd0b4a4e35d4a7a10",
+    "url": "/DaltonsReactPortfolioWeb/static/js/3.b52b6af5.chunk.js"
+  },
+  {
+    "revision": "2bdf225cfe3ac8555a42",
+    "url": "/DaltonsReactPortfolioWeb/static/js/main.6c5db60b.chunk.js"
+  },
+  {
+    "revision": "4af743ea52189e15562f",
+    "url": "/DaltonsReactPortfolioWeb/static/js/runtime~main.3e5425c4.js"
+  },
+  {
+    "revision": "13b667538b0c893cca7531acbcd0c12f",
+    "url": "/DaltonsReactPortfolioWeb/static/media/CamelbackWebsite.13b66753.jpg"
+  },
+  {
+    "revision": "8f28a545a0c5dc3c78c962aee89c0f48",
+    "url": "/DaltonsReactPortfolioWeb/static/media/DaltonsPortfolioPicture.8f28a545.jpg"
+  },
+  {
+    "revision": "60546e86935e85eee15b8d60f0967ca8",
+    "url": "/DaltonsReactPortfolioWeb/static/media/GlobalTechEx-logo.60546e86.png"
+  },
+  {
+    "revision": "556d1812443e7871670c7c81a9c5787d",
+    "url": "/DaltonsReactPortfolioWeb/static/media/HikingPicWebsite.556d1812.jpg"
+  },
+  {
+    "revision": "4bc35f96948031326c4f9989ffe74837",
+    "url": "/DaltonsReactPortfolioWeb/static/media/PortfolioHomeBackgroundImg.4bc35f96.jpeg"
+  },
+  {
+    "revision": "07996dc37d557db5f9b7f96a86add4b8",
+    "url": "/DaltonsReactPortfolioWeb/static/media/SedonaWebsite.07996dc3.jpg"
+  }
+]);
